c71c7ba2b1a3d14897bf6a274922c91a8ce1840c
vedhanth152 <126233177+vedhanth152@users.noreply.github.com>
2023-02-28 14:12:44 +0530

    sumofnumbers
    adding2numbers

---
+++ add1.java
@@ -0,0 +1,11 @@
+class add1
+{ 
+public static void main (String args[])
+{
+System.out.println("Addtion of two numbers");
+int a=10;
+int b=15;
+int c=a+b;
+System.out.println("result is"+c);
+}
+}
